12b03276f95c32c550eb725d85bdfe94adc98c81
chenye <15002770045@139.com>
2020-02-24 15:56:43 +0800

    Aop获取参数列表

---
+++ snow-system/src/main/java/com/stylesmile/aop/log/AopUtil.java
@@ -0,0 +1,34 @@
+package com.stylesmile.aop.log;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AopUtil {
+    /**
+     * 获取参数列表
+     *
+     * @param joinPoint
+     * @return
+     * @throws ClassNotFoundException
+     * @throws NoSuchMethodException
+     */
+    private static Map<String, Object> getFieldsName(ProceedingJoinPoint joinPoint) {
+        // 参数值
+        Object[] args = joinPoint.getArgs();
+        ParameterNameDiscoverer pnd = new DefaultParameterNameDiscoverer();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        String[] parameterNames = pnd.getParameterNames(method);
+        Map<String, Object> paramMap = new HashMap<>(32);
+        for (int i = 0; i < parameterNames.length; i++) {
+            paramMap.put(parameterNames[i], args[i]);
+        }
+        return paramMap;
+    }
+}
